e01797b61a5d5fe09ffd4cb6afcb04042e2067e7
Eric Martin <eric.martin@corellis.eu>
2018-08-10 17:41:10 +0200

    Fixing #129 again without breaking tests nor logic

--- src/index.js
+++ src/index.js
@@ -181,15 +181,15 @@ function createLoadableComponent(loadFn, options) {
         }, opts.timeout);
       }
 
-      let update = () => {
+      let update = (newState = {}) => {
         if (!this._mounted) {
           return;
         }
 
         this.setState({
-          error: res.error,
-          loaded: res.loaded,
-          loading: res.loading
+          error: newState.error || res.error,
+          loaded: newState.loaded || res.loaded,
+          loading: newState.loading || res.loading
         });
 
         this._clearTimeouts();
@@ -198,8 +198,13 @@ function createLoadableComponent(loadFn, options) {
       res.promise.then(() => {
         update();
       }).catch(err => {
+        // Throw any error caught after subsequent updates.
+        // Fixes #129
+        if (!res.error) {
+          update({ error: err, loading: false });
+          throw err;
+        }
         update();
-        throw err;
       });
     }
 
