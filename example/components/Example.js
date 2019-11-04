74cde5504bf7044a595d301200d2b5b570a21e9d
Velu S Gautam <i@velusgautam.com>
2019-11-04 15:26:28 +0100

    Removed some unused imports

--- example/components/Example.js
+++ example/components/Example.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import Loadable from 'react-loadable';
 import Loading from './Loading';
-import delay from '../utils/delay';
-import path from 'path';
 
 const LoadableNested = Loadable({
   loader: () => import('./ExampleNested'),
@@ -13,7 +11,7 @@ export default function Example() {
   return (
     <div>
       <h1>Hello from a loadable component</h1>
-      <LoadableNested/>
+      <LoadableNested />
     </div>
   );
 }
