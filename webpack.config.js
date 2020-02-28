4e34a7f4cdffbc6a44fed2523b96d9b8c57cf47f
Isac <isac_asj91@hotmail.com>
2020-02-28 19:25:39 -0300

    Upgrade Babel 7 (#151)
    * upgrade babel 7
    
    * remove node 4 support
    
    * update dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -21,13 +21,13 @@ module.exports = {
           options: {
             babelrc: false,
             presets: [
-              ['es2015', { modules: false }],
-              'react',
+              ['@babel/preset-env', { modules: false }],
+              '@babel/preset-react',
             ],
             plugins: [
               'syntax-dynamic-import',
-              'transform-class-properties',
-              'transform-object-assign',
+              '@babel/plugin-proposal-class-properties',
+              '@babel/plugin-transform-object-assign',
               require.resolve('./babel'),
             ],
           }
